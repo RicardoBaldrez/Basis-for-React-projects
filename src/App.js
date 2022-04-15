0d5db3d282f90b0392aa0faa777da7271a53a70d
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2022-04-15 01:20:57 -0300

    feat: 🎸 Application menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Login from './pages/Login';
+import Header from './components/Header';
 import GS from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
       <GS />
+      <Header />
       <Login />
     </>
   );
