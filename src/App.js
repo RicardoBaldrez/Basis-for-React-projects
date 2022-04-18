766087dae4a7cce1f4d822328ccf895c4db53fd7
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2022-04-18 20:37:40 -0300

    feat: 🎸 Router and History

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+import history from './services/history';
 import Header from './components/Header';
 import GS from './styles/GlobalStyles';
 import Routers from './routes';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <GS />
       <Header />
       <Routers />
-    </BrowserRouter>
+    </Router>
   );
 }
 
