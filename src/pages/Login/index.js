aab5b8589045aa0b22eef051c874efee59063627
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2022-04-19 15:25:08 -0300

    feat: 🎸 Axios configuration

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { toast } from 'react-toastify';
 
 import { Container } from '../../styles/GlobalStyles';
 
 import { Title } from './styled';
 
 export default function Login() {
-  const showMessage = () => {
-    toast.success('Success!!!');
-  };
-
   return (
     <Container>
       <Title>
@@ -17,9 +12,7 @@ export default function Login() {
         <small>Lorem, ipsum dolor sit.</small>
       </Title>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <button type="button" onClick={showMessage}>
-        aaa
-      </button>
+      <button type="button">aaa</button>
     </Container>
   );
 }
